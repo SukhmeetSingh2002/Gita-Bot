f09899d00a5f5764cc39f581b6bb8505b1f817fe
SukhmeetSingh <78161187+SukhmeetSingh2002@users.noreply.github.com>
2023-10-01 19:56:04 +0530

    minor changes

--- src/app.py
+++ src/app.py
@@ -4,6 +4,10 @@ from bot import BhagavadGitaBot
 app = Flask(__name__)
 bot = BhagavadGitaBot()
 
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({'message': 'Welcome to my Bhagavad Gita Bot!'})
+
 @app.route('/api/hello', methods=['GET'])
 def hello():
     return jsonify({'message': 'Hello, World!'})
@@ -15,7 +19,7 @@ def get_response():
     return jsonify({'response': response})
 
 if __name__ == '__main__':
-    if app.config['FLASK_ENV'] == 'development':
-        app.run(debug=True, host='0.0.0.0')
-    else:
-        app.run(host='0.0.0.0')
+    # if app.config['FLASK_ENV'] == 'development':
+    #     app.run(debug=True, host='0.0.0.0')
+    # else:
+    app.run(host='0.0.0.0')
